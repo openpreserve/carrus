f06e9266a511185b1036ea4358672f1f320ebca0
Alex Sychov <sankai1917@gmail.com>
2020-11-05 17:41:11 +0300

    added dire for py script and handled its execution

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import { stderr, stdin, stdout } from "process";
+import React, { useEffect, useState } from "react";
+
+const { exec } = require("child_process");
 
 const App = () => {
-  return <div>JHOVE 2020</div>;
+  const [output, setOutput] = useState("");
+
+  const runPythonScript = () => {
+    exec("python ./src/python/script.py", (error, stdout, stderr) => {
+      if (error) {
+        throw error;
+      }
+      setOutput(stdout);
+    });
+  };
+  return (
+    <div>
+      <span>
+        Click the button and the output of a py script will be shown here
+        ---&gt; {output}
+      </span>
+      <button onClick={() => runPythonScript()}>Click me</button>
+    </div>
+  );
 };
 
 export default App;
