c63c7a7134556cef7be4e62e9551104412d77fad
Nikita <NBogdanov17@yandex.by>
2020-12-07 09:50:13 +0300

    find one issue

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -69,7 +69,7 @@ const Main = props => {
         <Label for="action" className="mr-1 my-auto w-25">
           <span>{t('Action')}:</span>
         </Label>
-        <Input type="select" onChange={e => props.setAction(e.target.value)}>
+        <Input type="select" onChange={e => props.setAction(e.target.value)}> {/* при переходе на эту же страницу всегда открывется первый action независимо от того, какой выбран в редаксе */}
           {actions.map((e, i) => (
             <option key={i + 1 * 2}>{e.preservationActionName}</option>
           ))}
