b3feca0751da9a79cb8ab29979e5eeb2547ca928
mani1991-dev <>
2020-12-14 22:39:05 +0300

    fix setPar function for using on production build

--- src/utils/setPAR.js
+++ src/utils/setPAR.js
@@ -7,7 +7,6 @@ import path from 'path';
 import { readdirSync, readFileSync } from 'fs';
 
 export default async function setPAR(isDevelopment) {
-  const PAR = {};
   let actionsPath = path.join(__dirname, '..', 'src', 'PAR', 'Actions');
   let toolsPath = path.join(__dirname, '..', 'src', 'PAR', 'Tools');
   let optionsPath = path.join(__dirname, '..', 'src', 'PAR', 'Options');
@@ -15,27 +14,22 @@ export default async function setPAR(isDevelopment) {
     actionsPath = path.join(__dirname, '..', 'PAR', 'Actions');
     toolsPath = path.join(__dirname, '..', 'PAR', 'Tools');
     optionsPath = path.join(__dirname, '..', 'PAR', 'Options');
-    const actions = readdirSync(actionsPath).map(e => ({
-      ...JSON.parse(readFileSync(path.join(actionsPath, e), 'utf-8')),
-      active: false,
-    }));
-    const tools = readdirSync(toolsPath).map(e => ({
-      ...JSON.parse(readFileSync(path.join(toolsPath, e), 'utf-8')),
-      active: false,
-    }));
-    const options = readdirSync(optionsPath).map(e => ({
-      ...JSON.parse(readFileSync(path.join(optionsPath, e), 'utf-8')),
-      active: false,
-    }));
-    return {
-      actions,
-      tools,
-      options,
-    };
   }
-  PAR.actions = readdirSync(actionsPath).map(e => ({
+  const actions = readdirSync(actionsPath).map(e => ({
     ...JSON.parse(readFileSync(path.join(actionsPath, e), 'utf-8')),
     active: false,
   }));
-  return PAR;
+  const tools = readdirSync(toolsPath).map(e => ({
+    ...JSON.parse(readFileSync(path.join(toolsPath, e), 'utf-8')),
+    active: false,
+  }));
+  const options = readdirSync(optionsPath).map(e => ({
+    ...JSON.parse(readFileSync(path.join(optionsPath, e), 'utf-8')),
+    active: false,
+  }));
+  return {
+    actions,
+    tools,
+    options,
+  };
 }
