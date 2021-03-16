f7abd7c9ff3ecf3e419f8fe754c32d1e3d40be7a
Nikita <NBogdanov17@yandex.by>
2021-03-16 10:44:08 +0300

    increase report text width to 100%

--- src/components/Report/Report.js
+++ src/components/Report/Report.js
@@ -27,21 +27,21 @@ const Report = (props) => {
       <Jumbotron fluid className="bg-white pt-0 pb-0 m-0">
         <Container fluid className="d-flex flex-column align-items-center">
           <Container>
+            {report && path && (
+              <div className="d-flex flex-row mb-3 justify-content-end w-100">
+                <FileCopyIcon onClick={() => clipboard.writeText(report)} className="cursor-pointer" />
+                <FolderOpenIcon
+                  onClick={() => {
+                    if (os.platform() === 'linux') {
+                      showItemInFolder(path);
+                    } else shell.showItemInFolder(path);
+                  }}
+                  className="cursor-pointer"
+                />
+              </div>
+            )}
             <Jumbotron className="p-4 bg-light align-self-center m-0 d-flex flex-row">
-              <pre className="w-75 text-left">{report}</pre>
-              {report && path && (
-                <div className="d-flex flex-row ml-2 justify-content-end w-25">
-                  <FileCopyIcon onClick={() => clipboard.writeText(report)} className="cursor-pointer" />
-                  <FolderOpenIcon
-                    onClick={() => {
-                      if (os.platform() === 'linux') {
-                        showItemInFolder(path);
-                      } else shell.showItemInFolder(path);
-                    }}
-                    className="cursor-pointer"
-                  />
-                </div>
-              )}
+              <pre className="w-100 text-left">{report}</pre>
             </Jumbotron>
           </Container>
         </Container>
