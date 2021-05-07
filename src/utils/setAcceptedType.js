d47fe9f3f9eb618521a522df3dee046ece99d935
Nikita <NBogdanov17@yandex.by>
2021-05-07 07:08:46 +0300

    fix veraPdf

--- src/utils/setAcceptedType.js
+++ src/utils/setAcceptedType.js
@@ -1,3 +1,5 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable consistent-return */
 function flatDeep(arr, d = 1) {
   return d > 0 ? arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatDeep(val, d - 1) : val), [])
     : arr.slice();
