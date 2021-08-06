3d8d21f8fb5e21b1cae79881e77bdd62f34f336a
andreypraktika <andrey_praktika@mail.ru>
2021-08-06 10:03:38 +0300

    Extended default configuration

--- src/utils/setConfig.js
+++ src/utils/setConfig.js
@@ -53,9 +53,41 @@ export async function setConfig(isDevelopment, runJobFailed) {
           const defConfig = {
             defaultValues: {
               Identify: {
+                AIFF: {
+                  defaultAction: 'pronom only', defaultTool: 'FIDO',
+                },
+                GIF: {
+                  defaultAction: 'pronom only', defaultTool: 'FIDO',
+                },
+                HTML: {
+                  defaultAction: 'pronom only', defaultTool: 'FIDO',
+                },
+                Image: {
+                  defaultAction: 'pronom only', defaultTool: 'FIDO',
+                },
+                JPEG: {
+                  defaultAction: 'pronom only', defaultTool: 'FIDO',
+                },
+                JPEG2000: {
+                  defaultAction: 'pronom only', defaultTool: 'FIDO',
+                },
                 pdf: {
-                  defaultAction: 'pronom only',
-                  defaultTool: 'FIDO',
+                  defaultAction: 'pronom only', defaultTool: 'FIDO',
+                },
+                text: {
+                  defaultAction: 'pronom only', defaultTool: 'FIDO',
+                },
+                TIFF: {
+                  defaultAction: 'pronom only', defaultTool: 'FIDO',
+                },
+                WAVE: {
+                  defaultAction: 'pronom only', defaultTool: 'FIDO',
+                },
+                XML: {
+                  defaultAction: 'pronom only', defaultTool: 'FIDO',
+                },
+                zip: {
+                  defaultAction: 'pronom only', defaultTool: 'FIDO',
                 },
               },
             },
