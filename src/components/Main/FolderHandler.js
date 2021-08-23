339b8a699ee7a7cb0a67eb4827c1e4d3aeea5265
andreypraktika <andrey_praktika@mail.ru>
2021-08-23 14:07:28 +0300

    Completed batch tab w/o logic

--- src/components/Main/FolderHandler.js
+++ src/components/Main/FolderHandler.js
@@ -1,10 +1,11 @@
+/* eslint-disable import/no-duplicates */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from 'react';
-import { connect, useRef } from 'react-redux';
+import React, { useState, useEffect, useRef } from 'react';
+import { connect } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import Dropzone from 'react-dropzone';
 import { Jumbotron, Container } from 'reactstrap';
@@ -14,31 +15,50 @@ import MoveToInboxIcon from '@material-ui/icons/MoveToInbox';
 import { green } from '@material-ui/core/colors';
 import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
 import ImageOutlinedIcon from '@material-ui/icons/ImageOutlined';
+import { Nav, NavItem, NavLink, TabContent, TabPane, FormGroup, Label, Input, Button } from 'reactstrap';
 import {
   setFileInfo,
 } from '../../Redux/redux-reducers';
 import setAcceptedType from '../../utils/setAcceptedType';
 
 const FolderHandler = props => {
+  const batchDirRef = useRef();
   const { t } = useTranslation();
   const { fileName, mimeType, isTypeAccepted } = props;
   const [error, setError] = useState('');
-  // const directoryRef = useRef();
+  const [batchDir, setBatchDir] = useState('');
 
-  //   function getfolder() {
-  //     props.setDirPath(directoryRef.current.files[0].path);
-  //   }
+  function getFolder() {
+    setBatchDir(batchDirRef.current.files[0].path);
+  }
+  function checkboxHandleChange(e) {
+    console.log(e.target.checked);
+  }
 
   return (
     <div className="mt-3">
-      <label className="custom-file-upload ml-3">
-        <input
-          className="ml-3"
-          directory=""
-          webkitdirectory=""
-          type="file"
-        />
-        {t('SelectFolder')}
+      <FormGroup className="mt-3 w-100 d-flex flex-row align-items-center">
+        <Label for="customFile" className="mr-1 my-auto w-25">
+          {t('OutputFolder')}:
+        </Label>
+        <Input className="dir_path w-50" readOnly placeholder={batchDir} />
+        <div className="w-25 d-flex flex-row">
+          <label className="custom-file-upload ml-3">
+            <input
+              className="ml-3"
+              directory=""
+              webkitdirectory=""
+              type="file"
+              ref={batchDirRef}
+              onChange={getFolder}
+            />
+            {t('SelectFolder')}
+          </label>
+        </div>
+      </FormGroup>
+      <label>
+        <input type="checkbox" onChange={checkboxHandleChange} />
+        Recursive
       </label>
     </div>
   );
